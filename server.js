9ef4d7bead553c1b03ecfef87f78f73245f66739
Doy4625 <feeskers25@gmail.com>
2024-12-13 12:32:57 +0700

    lab7

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.APP_PORT || 5000;
 
+//DB connection
+const db = require('./app/models');
+db.sequelize.sync({force: false}).then(()=>{
+    console.log("DB Connected..");
+});
 //ROUTES
 require('./app/routes/user.route')(app);
 
